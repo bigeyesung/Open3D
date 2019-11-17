1f3af16ab86a738e36860a8aac4b3959acaa261f
contiliosung <sungchenhsi@gmail.com>
2019-11-17 15:52:54 +0000

    chanfe parameters values setting

--- src/Open3D/Camera/PinholeCameraIntrinsic.cpp
+++ src/Open3D/Camera/PinholeCameraIntrinsic.cpp
@@ -45,10 +45,10 @@ PinholeCameraIntrinsic::PinholeCameraIntrinsic(
 PinholeCameraIntrinsic::PinholeCameraIntrinsic(
         PinholeCameraIntrinsicParameters param) {
     if (param == PinholeCameraIntrinsicParameters::PrimeSenseDefault)
-        SetIntrinsics(640, 480, 525.0, 525.0, 319.5, 239.5);
+        SetIntrinsics(480, 480, 525.0, 525.0, 319.5, 239.5);
     else if (param ==
              PinholeCameraIntrinsicParameters::Kinect2DepthCameraDefault)
-        SetIntrinsics(512, 424, 365.456, 365.456, 254.878, 205.395);
+        SetIntrinsics(320, 424, 365.456, 365.456, 254.878, 205.395);
     else if (param ==
              PinholeCameraIntrinsicParameters::Kinect2ColorCameraDefault)
         SetIntrinsics(1920, 1080, 1059.9718, 1059.9718, 975.7193, 545.9533);
